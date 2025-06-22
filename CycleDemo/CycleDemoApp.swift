f625c2813b08f5e62f175f223e5fcfc5e4a375fc
AkitoAndo <28ww.lo.ol.ww28@gmail.com>
2025-06-22 10:33:29 +0900

    Initial Commit

---
+++ CycleDemo/CycleDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CycleDemoApp.swift
+//  CycleDemo
+//
+//  Created by Akito Ando on 2025/06/22.
+//
+
+import SwiftUI
+
+@main
+struct CycleDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CycleDemoTests/CycleDemoTests.swift
@@ -0,0 +1,16 @@
+//
+//  CycleDemoTests.swift
+//  CycleDemoTests
+//
+//  Created by Akito Ando on 2025/06/22.
+//
+
+import Testing
+
+struct CycleDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
